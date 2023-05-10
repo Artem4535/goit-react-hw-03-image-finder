9144e4e3991ec8770efd676b11d7c6c8d437476b
Artem Liaschenko <playfootball826@gmail.com>
2023-05-10 17:47:59 +0300

    change api

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -1,10 +1,5 @@
 export function FetchPhoto(inputValue, page) {
-  const key = '34736091-07dbf1d110e7bfeee2e88aa1e';
   const URL = `https://pixabay.com/api/?key=34736091-07dbf1d110e7bfeee2e88aa1e&q=${inputValue}&per_page=16$page=${page}`;
-  const options = {
-    q: inputValue,
-    page: page,
-  };
 
   return fetch(URL).then(response => {
     if (response.ok) {
